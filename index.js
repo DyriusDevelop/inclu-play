9e71a12480836ff1f70f897536c183abbe01bfc4
DyriusDevelop <richardsondlopes@hotmail.com>
2025-10-28 13:40:34 -0300

    feat: Codigo principal do sistema contendo configurações importantes

---
+++ index.js
@@ -0,0 +1,36 @@
+require("dotenv").config()
+const path = require("path")
+const express = require("express")
+
+// Configurando Express
+const app = express()
+app.use(express.json())
+app.set("views", path.join(__dirname, "public"))
+app.set("view engine", "ejs")
+app.engine("html", require("ejs").renderFile)
+
+// Registrando todas as rotas encontradas na pasta routes
+const routes = require("./router").loadAllRoutes()
+Object.entries(routes).forEach(([key, value]) => {
+    if (key === "get") {
+        app.get(value.uri, (request, response) => value.execute(request, response))
+    } else if (key === "post") {
+        app.set(value.uri, (request, response) => value.execute(request, response))
+    }
+})
+
+// Execução do programa
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Executando servidor em localhost:${process.env.PORT}`)
+})
+
+// Registrando evento de desligamento
+const shutdown = () => {
+    console.log("Desligando servidor...")
+    server.close(() => {
+        console.log("Servidor desligado.")
+        process.exit(0)
+    })
+}
+process.on("SIGINT", shutdown)
+process.on("SIGTERM", shutdown)
