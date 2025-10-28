5f66ef5e8ae2bf3c1b84b116c5d142b2312e4a00
DyriusDevelop <richardsondlopes@hotmail.com>
2025-10-28 13:39:13 -0300

    feat: Implementando logica de roteamento para que cada pagina tenha seu proprio arquivo

---
+++ router.js
@@ -0,0 +1,19 @@
+const path = require("path")
+const fs = require("fs")
+
+const loadAllRoutes = () => {
+    let rootPath = path.join(__dirname, "routes")
+    let routesFiles = fs.readdirSync(rootPath).filter(file => file.endsWith(".js"))
+    
+    let routes = {}
+    for (let file of routesFiles) {
+        let route = require(path.join(rootPath, file))
+        if ("method" in route && "uri" in route && "execute" in route) {
+            routes[route.method] = route
+        }
+    }
+
+    return routes
+}
+
+module.exports = { loadAllRoutes }
