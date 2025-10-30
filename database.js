fb68d743ad841f2447560cb5a1676a4f691ced17
DyriusDevelop <richardsondlopes@hotmail.com>
2025-10-29 21:21:40 -0300

    fix: Corrigindo criação de tabelas para serem criadas apenas quando não existir

--- database.js
+++ database.js
@@ -1,16 +1,30 @@
 const sqlite = require("sqlite3").verbose()
-const db = new sqlite.Database("./demo.db")
 
-// Configuração das tabelas do banco
-db.serialize(() => {
-    db.run(`CREATE TABLE clients (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        cpf INTEGER UNIQUE NOT NULL,
-        email TEXT UNIQUE NOT NULL,
-        password TEXT NOT NULL,
-        address TEXT NOT NULL
-    )`)
+// SQL para criação das tabelas
+const sqlUserTable = `CREATE TABLE IF NOT EXISTS clients (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    cpf INTEGER UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    address TEXT NOT NULL
+)`
+
+// Criação da conexão com o banco e criação das tabelas caso não existam
+const db = new sqlite.Database("./demo.db", (err) => {
+    if (err) {
+        console.error(`Erro ao abrir banco de dados : ${err.message}`)
+        return
+    }
+    console.log("Conectado ao banco de dados SQLite")
+})
+
+db.exec(sqlUserTable, (err) => {
+    if (err) {
+        console.error(`Erro ao tentar criar tabela 'clients' : ${err.message}`)
+    } else {
+        console.log("Tabela 'clients' criado com sucesso!")
+    }
 })
 
 module.exports = { db }
